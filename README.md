b996be39d2305a5171a3cb71b83b9a02f3b89e81
Robert Costa Santos <81938340+Ro-berth@users.noreply.github.com>
2024-08-22 21:05:08 +0000

    adiciona scripts  `lint:check` e  `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Conteúdos para desenvolver e melhora as habilidade na programação.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h2>Lena, eu amo você. Se você me ama, dá uma risadinha!!</h2>
+  return <h2>Lena, eu amo você. Se você me ama, dá uma risadinha!!</h2>;
+}
+function test2() {
+  console.log("teste");
 }
 
 export default Home;
